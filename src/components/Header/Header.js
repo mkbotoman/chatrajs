fc23f39642414b3d457e09f6a201ee6a02c8685d
mkbotoman <mkbotoman@gmail.com>
2015-07-15 21:29:23 -0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React from 'react';
+import styles from './Header.less';
+import withStyles from '../../decorators/withStyles';
+import Link from '../../utils/Link';
+import Navigation from '../Navigation';
+
+@withStyles(styles)
+class Header {
+
+  render() {
+    return (
+      <div className="Header">
+        <div className="Header-container">
+          <a className="Header-brand" href="/" onClick={Link.handleClick}>
+            <img className="Header-brandImg" src={require('./chatraHeader.png')} height="38" alt="Chatra" />
+          </a>
+          <Navigation className="Header-nav" />
+        </div>
+
+      </div>
+    );
+  }
+
+}
+
+export default Header;
